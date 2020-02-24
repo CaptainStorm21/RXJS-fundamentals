5a453d10fbd64a1ac799c42159dfd922ec20f6c4
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-23 23:01:53 -0800

    Create 1-immutable.js

---
+++ 1-immutable.js
@@ -0,0 +1,21 @@
+//immutable with function expression
+
+function double(a){
+  return a * 2;
+}
+
+function add(prev, a){
+  return prev + a;
+}
+
+//output is 13
+let res  = add(double(2), 9);
+console.log('sum', res);
+
+//output is 9
+res1 = add(double(2), 5);
+console.log('sum', res1);
+
+//output is 14
+res = add(double(2), 10);
+console.log('sum', res);
